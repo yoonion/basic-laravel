f5069e34bc084258be4bc2ddd637c3bf1f1f76de
yoonion <yoonion97@gmail.com>
2024-11-04 04:11:38 +0900

    refactor: 글 목록 blade 문법 적용하여 변경

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,15 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container p-5">
-        <h1 class="text-2xl mb-6">글 목록</h1>
-        <?php foreach ($articles as $article) : ?>
-            <div class="rounded border mb-3 p-3">
-                <p><?php echo $article->text; ?></p>
-                <p><?php echo $article->created_at; ?></p>
-                <p><?php echo $article->updated_at; ?></p>
-            </div>
-        <?php endforeach; ?>
-    </div>
+<div class="container p-5">
+    <h1 class="text-2xl mb-6">글 목록</h1>
+    @foreach($articles as $article)
+        <div class="rounded border mb-3 p-3">
+            <p>{{ $article->text }}</p>
+            <p>{{ $article->created_at }}</p>
+            <p>{{ $article->updated_at }}</p>
+        </div>
+    @endforeach
+</div>
 </body>
 </html>
