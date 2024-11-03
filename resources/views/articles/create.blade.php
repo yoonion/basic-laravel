89c6dda4230fc42e1d43ded7fc5ab54c72548013
yoonion <yoonion97@gmail.com>
2024-11-03 19:55:09 +0900

    feat: 글쓰기 view, 라우트 작성

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>글쓰기</h1>
+    <form action="/articles" method="post">
+        <input type="text" name="" id="">
+        <input type="button" value="저장하기">
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/articles/create', function () {
+    return view('articles/create');
+});
