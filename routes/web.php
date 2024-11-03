f7749f2f1c5b7ec97514bb56ce552dcfc9fa20b1
yoonion <yoonion97@gmail.com>
2024-11-04 03:04:09 +0900

    feat: php 만 이용한 글 저장

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,13 +27,31 @@ Route::get('/articles/create', function () {
 
 Route::post('/articles', function (Request $request) {
     // 비어있지 않고, 문자열이며, 255자를 넘지 않도록.
-    $request->validate([
+    $input = $request->validate([
         'text' => [
             'required',
             'string',
             'max:255'
         ],
     ]);
+
+    $host = config('database.connections.mysql.host');
+    $dbname = config('database.connections.mysql.database');
+    $username = config('database.connections.mysql.username');
+    $password = config('database.connections.mysql.password');
+
+    // pdo 객체를 만든다
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // 쿼리 준비
+    $stmt = $conn->prepare("INSERT INTO articles (text, user_id) VALUES (:body, :userId)");
+
+    // 쿼리 값 설정
+    $stmt->bindValue(':text', $input['text']);
+    $stmt->bindValue(':userId', Auth::id());
+
+    // 실행
+    $stmt->execute();
 
     return 'hello';
 });
